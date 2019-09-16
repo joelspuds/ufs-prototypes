8fa3c76dac140c676b210d80df9d2846ca891b08
Joel Spuds <joel.01@gmail.com>
2019-09-16 13:23:32 +0100

    tidying data source

--- dist/assets/javascripts/new-gds.bundle.js
+++ dist/assets/javascripts/new-gds.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/new-gds
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: September 16th 2019, 1:10:42 pm
+ *   Timestamp: September 16th 2019, 1:22:16 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
