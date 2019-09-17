9123d28d41b92d2178906523f387ec44e2d0f6f6
Joel Spuds <joel.01@gmail.com>
2019-09-17 16:49:17 +0100

    changing formatting for tinymce

--- dist/assets/javascripts/new-gds.bundle.js
+++ dist/assets/javascripts/new-gds.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/new-gds
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: September 17th 2019, 7:15:18 am
+ *   Timestamp: September 17th 2019, 4:09:50 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
