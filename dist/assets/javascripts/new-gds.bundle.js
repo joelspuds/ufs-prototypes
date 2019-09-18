abadb7a3f85d4366015425d5dc7d921b23f00e8b
Joel Spuds <joel.01@gmail.com>
2019-09-18 07:05:57 +0100

    rebuilding

--- dist/assets/javascripts/new-gds.bundle.js
+++ dist/assets/javascripts/new-gds.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/new-gds
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: September 17th 2019, 4:09:50 pm
+ *   Timestamp: September 18th 2019, 7:05:18 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
