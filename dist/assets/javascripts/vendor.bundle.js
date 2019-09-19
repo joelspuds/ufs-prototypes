82ad49e5efb41ccbd24bfe286ad8ac9d15504d4d
Joel Spuds <joel.01@gmail.com>
2019-09-19 10:57:15 +0100

    changing logo

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: September 18th 2019, 4:01:35 pm
+ *   Timestamp: September 19th 2019, 10:55:09 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
