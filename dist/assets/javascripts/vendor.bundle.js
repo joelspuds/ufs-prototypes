7835fdfd17b9aa774574845dad73ea887996b061
Joel Spuds <joel.01@gmail.com>
2019-11-11 09:00:31 +0000

    adding dirty date-time hack thing

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: November 5th 2019, 8:45:26 am
+ *   Timestamp: November 11th 2019, 8:50:15 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
