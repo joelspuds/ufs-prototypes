81d64ae06ea83c4f775e766a87e35434882a7cab
Joel Spuds <joel.01@gmail.com>
2019-11-19 09:35:19 +0000

    updating col layouts

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: November 18th 2019, 3:39:09 pm
+ *   Timestamp: November 19th 2019, 9:34:35 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
