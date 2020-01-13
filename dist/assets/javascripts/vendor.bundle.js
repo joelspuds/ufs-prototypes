d28f43fa3373c33dec9a89bc957ab0aa6ef11b55
Joel Spuds <joel.01@gmail.com>
2020-01-13 13:00:05 +0000

    making mvp

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: January 10th 2020, 10:16:33 am
+ *   Timestamp: January 13th 2020, 12:58:00 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
