c24585419432f8adb5f06713ba871d993e7747cf
Joel Spuds <joel.01@gmail.com>
2020-02-13 11:38:33 +0000

    tidying application items

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 13th 2020, 10:41:11 am
+ *   Timestamp: February 13th 2020, 11:27:03 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
