25fc86c50a1a02d590b369331d497b49bfa6cb66
Joel Spuds <joel.01@gmail.com>
2020-02-13 10:07:26 +0000

    progress bar at zero

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 12th 2020, 11:05:18 am
+ *   Timestamp: February 13th 2020, 10:06:48 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
