1bd5489b74d38257863a5e4ca9b90e5476d5c3ca
Joel Spuds <joel.01@gmail.com>
2020-02-14 11:48:32 +0000

    adding ewrror states and header fix

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 13th 2020, 11:27:03 am
+ *   Timestamp: February 14th 2020, 11:44:20 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
