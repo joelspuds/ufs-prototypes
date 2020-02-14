f3a119afa46a4be15dc13b61dc76e3c00b56a988
Joel Spuds <joel.01@gmail.com>
2020-02-14 13:35:18 +0000

    fixed icons

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 14th 2020, 1:06:57 pm
+ *   Timestamp: February 14th 2020, 1:34:46 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
