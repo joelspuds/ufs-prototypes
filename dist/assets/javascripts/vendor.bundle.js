c23c6e27eaac912680f75f683548c5451b21009a
Joel Spuds <joel.01@gmail.com>
2020-02-17 14:42:05 +0000

    fixing tunymce

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: February 17th 2020, 2:23:51 pm
+ *   Timestamp: February 17th 2020, 2:41:26 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
