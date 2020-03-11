c62680904806575911155c3606c3ccf829464f61
Joel Spuds <joel.01@gmail.com>
2020-03-11 10:39:07 +0000

    adding new app item row and top user nav

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 10th 2020, 11:47:50 am
+ *   Timestamp: March 11th 2020, 10:38:11 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
