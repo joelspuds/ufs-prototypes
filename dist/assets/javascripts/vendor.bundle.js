fc4f6aeaedf5bfc69b54ed694e1fdbcdf264d06a
Joel Spuds <joel.01@gmail.com>
2020-03-17 08:38:06 +0000

    pagination spacing

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 17th 2020, 7:44:16 am
+ *   Timestamp: March 17th 2020, 8:34:54 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
