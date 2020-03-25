41f62c96fdf39188817d2af2b1475dcb1c51dcb3
Joel Spuds <joel.01@gmail.com>
2020-03-25 12:34:16 +0000

    fixing image path

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 25th 2020, 12:28:24 pm
+ *   Timestamp: March 25th 2020, 12:33:39 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
