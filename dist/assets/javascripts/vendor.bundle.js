c7eaa6c337bd3dc82d71571ec3f2a519f99a86ee
Joel Spuds <joel.01@gmail.com>
2020-03-26 14:34:25 +0000

    new header

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 26th 2020, 6:34:05 am
+ *   Timestamp: March 26th 2020, 2:33:37 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
