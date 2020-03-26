2a4621e4a66ec68dd1c048d8b49c565730c43a2a
Joel Spuds <joel.01@gmail.com>
2020-03-26 06:34:56 +0000

    fixing emails

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 26th 2020, 6:17:23 am
+ *   Timestamp: March 26th 2020, 6:34:05 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
