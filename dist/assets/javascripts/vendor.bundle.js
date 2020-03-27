d84ff642b042502532c27688a48b75d679bc61ef
Joel Spuds <joel.01@gmail.com>
2020-03-27 14:39:08 +0000

    adding address to emails

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: March 27th 2020, 6:53:47 am
+ *   Timestamp: March 27th 2020, 2:38:17 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
