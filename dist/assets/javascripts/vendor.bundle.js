17ff5ec8e6e6f0fb009c32529be54674fb049b5e
Joel Spuds <joel.01@gmail.com>
2020-04-16 11:05:23 +0100

    removing overview label title thing

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: April 16th 2020, 10:16:17 am
+ *   Timestamp: April 16th 2020, 11:02:48 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
