b2ed1065359a287b60eef8792424d5d0578f3690
Joel Spuds <joel.01@gmail.com>
2020-05-13 17:13:28 +0100

    tons widget thing

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: May 13th 2020, 12:45:22 pm
+ *   Timestamp: May 13th 2020, 5:12:58 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
