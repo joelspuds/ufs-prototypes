d6245a2c1f5f63f68719043f2fd219b84f0f8d75
Joel Spuds <joel.01@gmail.com>
2020-05-19 16:15:50 +0100

    fixing Tom's responsive thing

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: May 19th 2020, 1:56:30 pm
+ *   Timestamp: May 19th 2020, 4:14:46 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
