0c72391f379fd1cf48fb33cae624b6ee3da63297
Joel Spuds <joel.01@gmail.com>
2020-05-21 15:58:09 +0100

    fixing back links

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: May 21st 2020, 3:19:28 pm
+ *   Timestamp: May 21st 2020, 3:57:43 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
