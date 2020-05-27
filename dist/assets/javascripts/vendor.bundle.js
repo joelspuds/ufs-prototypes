7ead28709061a6ea044de82c8be6e1747b33f08c
Joel Spuds <joel.01@gmail.com>
2020-05-27 10:40:27 +0100

    404 error

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: May 27th 2020, 8:18:44 am
+ *   Timestamp: May 27th 2020, 10:33:25 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
