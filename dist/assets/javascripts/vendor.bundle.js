6aeec1f53aadb3928aad6368d04f2a099c8f1ef1
Joel Spuds <joel.01@gmail.com>
2020-05-27 08:19:07 +0100

    fixing print link

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: May 26th 2020, 2:10:28 pm
+ *   Timestamp: May 27th 2020, 8:18:44 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
