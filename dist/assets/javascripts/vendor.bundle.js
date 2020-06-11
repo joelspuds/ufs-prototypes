a75829498cd2610bfcdcc2f0763d1e16cd98a0ea
Joel Spuds <joel.01@gmail.com>
2020-06-11 14:44:31 +0100

    adding admin views of application

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: June 10th 2020, 3:04:27 pm
+ *   Timestamp: June 11th 2020, 2:42:44 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
