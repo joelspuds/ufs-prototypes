0c095358045f65e37e2bd023687208823aac3595
Joel Spuds <joel.01@gmail.com>
2020-06-12 14:26:57 +0100

    adding dashboard pages

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: June 12th 2020, 8:30:30 am
+ *   Timestamp: June 12th 2020, 2:26:03 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
