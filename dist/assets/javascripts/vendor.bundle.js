36b813641ac30f74c1b33fd70b4da50531b67520
Joel Spuds <joel.01@gmail.com>
2020-06-15 10:26:21 +0100

    tweaking skip target

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: June 15th 2020, 9:47:10 am
+ *   Timestamp: June 15th 2020, 10:23:39 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
