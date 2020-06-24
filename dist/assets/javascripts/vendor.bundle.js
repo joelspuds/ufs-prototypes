e50b629c166fd99361a2ba9ec63da828f9a4c487
Joel Spuds <joel.01@gmail.com>
2020-06-24 13:57:56 +0100

    tweaking spacing

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: June 24th 2020, 1:19:58 pm
+ *   Timestamp: June 24th 2020, 1:57:20 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
