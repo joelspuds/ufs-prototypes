07a8f88f0e27210f7a0dd7c1ba03c09c859c1292
Joel Spuds <joel.01@gmail.com>
2020-06-30 13:41:49 +0100

    font size in view mode

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: June 30th 2020, 1:37:05 pm
+ *   Timestamp: June 30th 2020, 1:41:29 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
