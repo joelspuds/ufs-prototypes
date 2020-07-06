c2dd1c7d78781daabda0d6d9dbd9dbfc89b31876
Joel Spuds <joel.01@gmail.com>
2020-07-06 09:02:25 +0100

    2483 error content

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: July 2nd 2020, 4:00:17 pm
+ *   Timestamp: July 6th 2020, 9:01:44 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
