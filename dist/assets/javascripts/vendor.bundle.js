5913dd80e9536177f42c7a556e16d0123c265788
Joel Spuds <joel.01@gmail.com>
2020-07-08 12:46:10 +0100

    adding switchy cols

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: July 8th 2020, 9:48:59 am
+ *   Timestamp: July 8th 2020, 12:45:04 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
