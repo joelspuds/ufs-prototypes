7400a50e52e4558aa7110b79f2f0b99b607234bc
Joel Spuds <joel.01@gmail.com>
2020-08-11 14:55:02 +0100

    fixing header menu links and sign out

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: August 7th 2020, 10:02:23 am
+ *   Timestamp: August 11th 2020, 2:54:21 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
