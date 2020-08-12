e19ce93dffc2914f5f3c0ad5c52cd769cc99640a
Joel Spuds <joel.01@gmail.com>
2020-08-12 15:15:38 +0100

    adding tables to tinymce

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: August 12th 2020, 2:33:13 pm
+ *   Timestamp: August 12th 2020, 3:15:14 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
