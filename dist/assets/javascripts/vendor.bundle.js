f2040262f3704991b795eac4dde4bdbfcedc8feb
Joel Spuds <joel.01@gmail.com>
2020-08-13 13:58:50 +0100

    changing email font size

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: August 13th 2020, 1:21:41 pm
+ *   Timestamp: August 13th 2020, 1:58:29 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
