41b1259ea3fa8f8189660b1234510fcd04daf23b
Joel Spuds <joel.01@gmail.com>
2020-08-13 11:51:38 +0100

    email link style edit

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: August 13th 2020, 10:39:44 am
+ *   Timestamp: August 13th 2020, 11:50:05 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
