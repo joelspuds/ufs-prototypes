bf9f84fa6043f787159ad8a61946fa5c4baad41e
Joel Spuds <joel.01@gmail.com>
2020-08-13 10:40:11 +0100

    updaing emails and molecules index

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: August 12th 2020, 3:15:14 pm
+ *   Timestamp: August 13th 2020, 10:39:44 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
