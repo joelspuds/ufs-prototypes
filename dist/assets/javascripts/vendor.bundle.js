234d5e695a053f8dd0709d95cc4e93b35cf75c5a
Joel Spuds <joel.01@gmail.com>
2020-08-17 10:28:44 +0100

    aria stuff in pagination

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: August 14th 2020, 2:16:01 pm
+ *   Timestamp: August 17th 2020, 10:27:47 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
