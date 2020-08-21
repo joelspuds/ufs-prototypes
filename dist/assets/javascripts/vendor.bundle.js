c4e048e789c76dc1bb8e42d0c42e6ef7b6e17b4d
Joel Spuds <joel.01@gmail.com>
2020-08-21 10:03:50 +0100

    outline on task button

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: August 21st 2020, 8:37:52 am
+ *   Timestamp: August 21st 2020, 9:31:09 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
