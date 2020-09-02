8b674626d3263b8410026052c4f45883097ac930
Joel Spuds <joel.01@gmail.com>
2020-09-02 13:38:42 +0100

    tweaking col widths in applciants

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: September 2nd 2020, 11:43:04 am
+ *   Timestamp: September 2nd 2020, 1:38:08 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
