a838631b1010cf778358618a356d269b18bde65e
Joel Spuds <joel.01@gmail.com>
2020-09-02 11:43:48 +0100

    adding appliucants page

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: August 26th 2020, 1:12:29 pm
+ *   Timestamp: September 2nd 2020, 11:43:04 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
