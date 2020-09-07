9feaf1b78b3ca21bd59b2a37bd141e6cae27a8e6
Joel Spuds <joel.01@gmail.com>
2020-09-07 10:21:17 +0100

    more column options

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: September 7th 2020, 10:07:23 am
+ *   Timestamp: September 7th 2020, 10:20:28 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
