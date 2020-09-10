5eab7f5fbb9d307362db460f5109c4ab05149f5f
Joel Spuds <joel.01@gmail.com>
2020-09-10 11:17:06 +0100

    resources table errors

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: September 8th 2020, 9:33:12 am
+ *   Timestamp: September 10th 2020, 11:16:33 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
