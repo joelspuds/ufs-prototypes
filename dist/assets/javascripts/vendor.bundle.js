ab1c3b8e83e250a636acaea0414b09d29ca28826
Joel Spuds <joel.01@gmail.com>
2020-09-11 12:24:09 +0100

    fixed js rounding values

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: September 11th 2020, 12:04:02 pm
+ *   Timestamp: September 11th 2020, 12:23:48 pm
  *   Source: https://github.com/dvsa/front-end
  * 
  */
