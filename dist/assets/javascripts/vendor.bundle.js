a8a60ec8762d5dc9171b6caa1766088703bd4fa9
Joel Spuds <joel.01@gmail.com>
2020-10-27 11:02:53 +0000

    col width for email in applicants page

--- dist/assets/javascripts/vendor.bundle.js
+++ dist/assets/javascripts/vendor.bundle.js
@@ -3,7 +3,7 @@
  *   Name: dvsa-front-end/vendor
  *   Version: 1.3.15
  *   Contributors: Martin D Marriott <martind@kainos.com>, James Nelson <j.nelson@kainos.com>, Tameem Safi <https://github.com/tameemsafi>
- *   Timestamp: October 26th 2020, 2:23:34 pm
+ *   Timestamp: October 27th 2020, 11:02:29 am
  *   Source: https://github.com/dvsa/front-end
  * 
  */
